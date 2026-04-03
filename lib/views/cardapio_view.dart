9d58f6514d0288b636b76c1859744c58c4b913f4
GVerso <112283257+GabrielReverso@users.noreply.github.com>
2026-04-03 17:04:52 -0300

    Navbar e navegação entre seções

--- lib/views/cardapio_view.dart
+++ lib/views/cardapio_view.dart
@@ -1,109 +1,180 @@
 import 'package:flutter/material.dart';
 
-class CardapioView extends StatelessWidget {
+class CardapioView extends StatefulWidget {
   const CardapioView({super.key});
 
   @override
+  State<CardapioView> createState() => _CardapioViewState();
+}
+
+class _CardapioViewState extends State<CardapioView> {
+  final ScrollController _controller = ScrollController();
+
+  static const double headerHeight = 150;
+  static const double navbarHeight = 100;
+
+  double scrollOffset = 0;
+
+  // TODO: Keys dinamicas via API
+  // keys das seções
+  final List<GlobalKey> sectionKeys = List.generate(5, (_) => GlobalKey());
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller.addListener(() {
+      setState(() {
+        scrollOffset = _controller.offset;
+      });
+    });
+  }
+
+  /* Scroll para seção do cardápio */
+  void scrollToSection(int index) {
+    final context = sectionKeys[index].currentContext;
+
+    if (context == null) return;
+
+    final box = context.findRenderObject() as RenderBox;
+
+    final position = box.localToGlobal(Offset.zero);
+
+    final offset = position.dy + _controller.offset;
+
+    // compensa header + navbar overlay
+    final target = offset - headerHeight - navbarHeight;
+
+    _controller.animateTo(
+      target,
+      duration: const Duration(milliseconds: 400),
+      curve: Curves.easeInOut,
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final statusBar = MediaQuery.of(context).padding.top;
+
+    final navbarTopPosition = headerHeight - scrollOffset;
+
+    final top = navbarTopPosition
+        .clamp(statusBar, double.infinity)
+        .roundToDouble();
+
+    final isSticky = top == statusBar;
+
     return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          // HEADER
-          SliverToBoxAdapter(
-            child: Container(
-              height: 150,
-              color: Colors.red.shade200,
-              alignment: Alignment.center,
-              child: const Text('HEADER', style: TextStyle(fontSize: 24)),
+      body: Stack(
+        children: [
+          // SCROLL PRINCIPAL
+          SingleChildScrollView(
+            controller: _controller,
+            child: Column(
+              children: [
+                // HEADER
+                Container(
+                  height: headerHeight,
+                  color: Colors.red.shade200,
+                  alignment: Alignment.center,
+                  child: const Text('HEADER', style: TextStyle(fontSize: 24)),
+                ),
+
+                // BODY com padding
+                Padding(
+                  padding: const EdgeInsets.only(top: navbarHeight),
+                  child: Column(
+                    children: List.generate(5, (sectionIndex) {
+                      return Container(
+                        key: sectionKeys[sectionIndex],
+                        margin: const EdgeInsets.symmetric(vertical: 16),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            // título da seção
+                            Padding(
+                              padding: const EdgeInsets.all(8),
+                              child: Text(
+                                'Seção $sectionIndex',
+                                style: const TextStyle(
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+
+                            // 4 itens
+                            ...List.generate(
+                              4,
+                              (i) => Container(
+                                height: 80,
+                                margin: const EdgeInsets.all(8),
+                                color: Colors.orange.shade200,
+                                alignment: Alignment.center,
+                                child: Text(
+                                  'Item $sectionIndex - $i',
+                                  style: const TextStyle(fontSize: 16),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                    }),
+                  ),
+                ),
+              ],
             ),
           ),
 
-          // NAVBAR STICKY
-          SliverPersistentHeader(pinned: true, delegate: _NavbarDelegate()),
+          // NAVBAR
+          Positioned(
+            top: top - 1,
+            left: 0,
+            right: 0,
+            child: Container(
+              height: navbarHeight,
+              decoration: BoxDecoration(
+                color: Colors.blue.shade200,
+                boxShadow: isSticky
+                    ? [
+                        const BoxShadow(
+                          blurRadius: 8,
+                          offset: Offset(0, 2),
+                          color: Colors.black26,
+                        ),
+                      ]
+                    : [],
+              ),
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: 5,
+                itemBuilder: (context, index) {
+                  return GestureDetector(
+                    onTap: () => scrollToSection(index),
+                    child: Container(
+                      width: 120,
+                      margin: const EdgeInsets.all(8),
+                      color: Colors.blue.shade400,
+                      alignment: Alignment.center,
+                      child: Text(
+                        'Seção $index',
+                        style: const TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
 
-          // BODY DINÂMICO
-          SliverList(
-            delegate: SliverChildBuilderDelegate((context, index) {
-              return Container(
-                height: 80,
-                margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
-                color: Colors.orange.shade200,
-                alignment: Alignment.center,
-                child: Text(
-                  'Item $index',
-                  style: const TextStyle(fontSize: 18),
-                ),
-              );
-            }, childCount: 30),
+          // STATUS BAR
+          Container(
+            height: statusBar,
+            color: isSticky ? Colors.blue.shade200 : Colors.red.shade200,
           ),
         ],
       ),
     );
-  }
-}
-
-// DELEGATE DA NAVBAR
-class _NavbarDelegate extends SliverPersistentHeaderDelegate {
-  static const double navbarHeight = 100;
-
-  @override
-  double get minExtent => navbarHeight;
-
-  @override
-  double get maxExtent =>
-      navbarHeight + WidgetsBinding.instance.window.padding.top;
-
-  @override
-  Widget build(
-    BuildContext context,
-    double shrinkOffset,
-    bool overlapsContent,
-  ) {
-    final topPadding = MediaQuery.of(context).padding.top;
-
-    // progresso (0 → 1)
-    final t = (shrinkOffset / topPadding).clamp(0.0, 1.0);
-
-    final extraTop = topPadding * t;
-
-    return Stack(
-      children: [
-        // 🔵 FUNDO (cresce pra cima)
-        Container(
-          color: Colors.blue.shade200,
-        ),
-
-        // 🔥 NAVBAR FIXA (não é empurrada)
-        Positioned(
-          top: extraTop,
-          left: 0,
-          right: 0,
-          child: SizedBox(
-            height: navbarHeight + extraTop,
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: 20,
-              itemBuilder: (context, index) {
-                return Container(
-                  width: 120,
-                  margin: const EdgeInsets.all(8),
-                  color: Colors.blue.shade400,
-                  alignment: Alignment.center,
-                  child: Text(
-                    'Tab $index',
-                    style: const TextStyle(color: Colors.white),
-                  ),
-                );
-              },
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-
-  @override
-  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) {
-    return true;
   }
 }
